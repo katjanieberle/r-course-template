2d969739ca38eb59609db7aaad62454afa4c5194
katjanieberle <katja.nieberle@gmail.com>
2026-04-28 22:03:48 +0200

    random distributions

--- scripts/exercise3_code.R
+++ scripts/exercise3_code.R
@@ -1,13 +1,13 @@
 # wo bin ich?
 
 getwd()
-
-
+#-------------------------------------------------------------------------------
 # vergleiche !
 
 read.csv2("data//input//oktoberfest.csv")
 
 read.csv2("data//input//oktoberfest.csv", sep=",")
+#-------------------------------------------------------------------------------
 
 # 1. Analysiere den Data Frame
 
@@ -15,7 +15,7 @@ read.csv2("data//input//oktoberfest.csv", sep=",")
 
 # 3. Visualisiere den Bierkonsum und Bierpreise, sowie Hendlkonsum und Hendlpreise
 
-
+#-------------------------------------------------------------------------------
 
 # Bierkonsum und Bierpreise
 dff <- read.csv2("data//input//oktoberfest.csv", sep=",")
@@ -46,4 +46,13 @@ legend("topleft",
        col = c("blue", "red"),
        lwd = 2,
        bty = "n")
+#-------------------------------------------------------------------------------
+
+# Generiere 1000 binomialverteilten Zufallsvariablen mit p = 0.15 und stelle sie grafisch dar
+
+#-------------------------------------------------------------------------------
+
+# Generiere 1000 poissonverteilten Zufallsvariablen mit lambda = 1.2 und stelle sie grafisch dar
+
+#-------------------------------------------------------------------------------
 
