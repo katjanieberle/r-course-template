4c876ac203cc3c093a77d2c1b646f9895437e1cf
katjanieberle <katja.nieberle@gmail.com>
2026-05-05 21:42:06 +0200

    stetige zv

---
+++ scripts/exercise4_code.R
@@ -0,0 +1,48 @@
+#-------------------------------------------------------------------------------
+# Gleichverteilung
+#-------------------------------------------------------------------------------
+# Sei X stetig gleichverteilt auf [2,6]. 
+#
+# 1) Wie hoch ist die Wahrscheinlichkeit P(3 ≤ X ≤ 5)?
+# 2) Wie hoch ist die Wahrscheinlichkeit P(2 < X < 7)?
+# 3) Wie hoch ist die Wahrscheinlichkeit P(4 < X < 5.4)?
+# 4) Wie groß ist der Erwartungswert E(X) und die Varianz V(X)?
+
+
+#-------------------------------------------------------------------------------
+# Normalverteilung 
+#-------------------------------------------------------------------------------
+# Der Körpergröße einer erwachsenen Bevölkerungsgruppe kann näherungsweise 
+# eine Normalverteilung mit Mittelwert 175 cm und Standardabweichung 7 cm
+# zugeschrieben werden. 
+
+# 1) Wie hoch ist der Anteil an Personen mit der Körpergröße zwischen 168 cm und 180 cm?
+# 2) Wie hoch ist der Anteil an Personen mit der Körpergröße > 180 cm?
+# 3) Wie hoch ist der Anteil an Personen mit der Körpergröße < 160 cm?
+
+
+#-------------------------------------------------------------------------------
+# Normalverteilung 
+#-------------------------------------------------------------------------------
+# Gegeben ist die normal verteilte Zufallsvariable X ~ N(10, 4)
+# 1. Simulieren Sie 1000 N(10,4) verteilten Zufallsvariablen
+# 2. Bilden Sie aus den 1000 Variablen (Schritt 1) die standardnormal verteilten 
+# Zufallsvariablen Z
+# 3. Zeigen Sie grafisch mit Hilfe eines Histogramms die Verteilungen 
+# von X (Schritt 1) und Z (Schritt 2). Was stellen Sie fest?
+
+
+
+#-------------------------------------------------------------------------------
+# Gesetz der Großen Zahlen
+#-------------------------------------------------------------------------------
+# 0. seed Setzen
+
+# 1. X: Simulieren Sie 100 binomial verteilte Zufallsvariablen mit size=20, prob = 0.3
+
+# 2. Y: Simulieren Sie 1000 binomial verteilte Zufallsvariablen mit size=20, prob = 0.3
+
+# 3. Z: Simulieren Sie 100000 binomial verteilte Zufallsvariablen mit size=20, prob = 0.3
+
+
+
