8ed7321b8c0b1a42e2601845305cf48bc7087bea
katjanieberle <katja.nieberle@gmail.com>
2026-05-12 22:33:22 +0200

    Approximationen

---
+++ scripts/exercise5_code.R
@@ -0,0 +1,34 @@
+#-------------------------------------------------------------------------------
+# Approximationen von Verteilungen
+#-------------------------------------------------------------------------------
+
+# Aufgabe 1: X ist Poisson verteilt mit lambda = 10. 
+
+# Frage 1: Wie hoch ist die Wahrscheinlichkeit für X = 7?
+
+# 1) Berechnen Sie die Wahrscheinlichkeit mit Hilfe der Poisson Verteilung.
+# 2) Berechnen Sie die Wahrscheinlichkeit mit Hilfe der Normalverteilung. 
+# Nutzen Sie die Stetigkeitskorrektur
+
+# Frage 2: Wie hoch ist die Wahrscheinlichkeit für X <= 7?
+
+# 1) Berechnen Sie die Wahrscheinlichkeit mit Hilfe der Poisson Verteilung.
+# 2) Berechnen Sie die Wahrscheinlichkeit mit Hilfe der Normalverteilung. 
+# Nutzen Sie die Stetigkeitskorrektur
+
+#-------------------------------------------------------------------------------
+
+# X ist Binomal verteilt mit n = 20 und pi = 0.4. 
+
+# Frage 1: Wie hoch ist die Wahrscheinlichkeit für X = 13?
+
+# 1) Berechnen Sie die Wahrscheinlichkeit mit Hilfe der Binomialverteilung.
+# 2) Berechnen Sie die Wahrscheinlichkeit mit Hilfe der Normalverteilung. 
+# Nutzen Sie die Stetigkeitskorrektur
+
+# Frage 2: Wie hoch ist die Wahrscheinlichkeit für X zwischen 10 und 17?
+
+# 1) Berechnen Sie die Wahrscheinlichkeit mit Hilfe der Binomialverteilung.
+# 2) Berechnen Sie die Wahrscheinlichkeit mit Hilfe der Normalverteilung. 
+# Nutzen Sie die Stetigkeitskorrektur
+
