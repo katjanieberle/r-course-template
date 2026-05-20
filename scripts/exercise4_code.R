5ff717a3682ee1c39b255eee9cc6c9645c9921fe
katjanieberle <katja.nieberle@gmail.com>
2026-05-20 21:05:04 +0200

    add calculations

--- scripts/exercise4_code.R
+++ scripts/exercise4_code.R
@@ -1,13 +1,34 @@
 #-------------------------------------------------------------------------------
 # Gleichverteilung
 #-------------------------------------------------------------------------------
+#p - Verteilungsfunktion
+#d - Dichtefunktion
+#r - Zufallszahlen generieren
+#q - Quantile
+
+# unif
+# norm
 # Sei X stetig gleichverteilt auf [2,6]. 
 #
 # 1) Wie hoch ist die Wahrscheinlichkeit P(3 ≤ X ≤ 5)?
-# 2) Wie hoch ist die Wahrscheinlichkeit P(2 < X < 7)?
-# 3) Wie hoch ist die Wahrscheinlichkeit P(4 < X < 5.4)?
-# 4) Wie groß ist der Erwartungswert E(X) und die Varianz V(X)?
 
+# P(X <= 5) - P(X <= 3) = 
+punif(q = 5, min = 2, max = 6) - punif(q = 3, min=2, max=6)
+
+# 2) Wie hoch ist die Wahrscheinlichkeit P(2 < X < 7)?
+punif(q = 7, min = 2, max = 6) - punif(q = 2, min=2, max=6)
+
+# 3) Wie hoch ist die Wahrscheinlichkeit P(4 < X < 5.4)?
+punif(q = 5.4, min = 2, max = 6) - punif(q = 4, min=2, max=6)
+
+# 4) Wie groß ist der Erwartungswert E(X) und die Varianz V(X)?
+# (a+b)/2
+expct_value <- (2+6)/2
+expct_value
+
+# (b-a)^2/12
+var_value <- (6 - 2)^2/12
+var_value
 
 #-------------------------------------------------------------------------------
 # Normalverteilung 
@@ -17,20 +38,35 @@
 # zugeschrieben werden. 
 
 # 1) Wie hoch ist der Anteil an Personen mit der Körpergröße zwischen 168 cm und 180 cm?
+# P(X <= 180) - P(X <= 168)
+pnorm(q=180, mean = 175, sd = 7) - pnorm(q=168, mean = 175, sd = 7) 
+
 # 2) Wie hoch ist der Anteil an Personen mit der Körpergröße > 180 cm?
+# P(X > 180) = 1 - P(X <= 180)
+1 - pnorm(q = 180, mean = 175, sd = 7) 
+
 # 3) Wie hoch ist der Anteil an Personen mit der Körpergröße < 160 cm?
-
-
+# P(X < 160)
+pnorm(q = 160, mean = 175, sd = 7) 
 #-------------------------------------------------------------------------------
 # Normalverteilung 
 #-------------------------------------------------------------------------------
 # Gegeben ist die normal verteilte Zufallsvariable X ~ N(10, 4)
 # 1. Simulieren Sie 1000 N(10,4) verteilten Zufallsvariablen
+?rnorm
+set.seed(454564)
+zv_10_4 <- rnorm(n = 1000, mean = 10, sd = 2)
+
 # 2. Bilden Sie aus den 1000 Variablen (Schritt 1) die standardnormal verteilten 
 # Zufallsvariablen Z
+zv_0_1 <- (zv_10_4 - 10)/2
+
 # 3. Zeigen Sie grafisch mit Hilfe eines Histogramms die Verteilungen 
 # von X (Schritt 1) und Z (Schritt 2). Was stellen Sie fest?
 
+par(mfrow=c(1,2))
+hist(zv_10_4)
+hist(zv_0_1)
 
 
 #-------------------------------------------------------------------------------
@@ -40,9 +76,35 @@
 
 # 1. X: Simulieren Sie 100 binomial verteilte Zufallsvariablen mit size=20, prob = 0.3
 
+set.seed(23487256)
+xx <- rbinom(n = 10, size =20, prob=0.3)
+
 # 2. Y: Simulieren Sie 1000 binomial verteilte Zufallsvariablen mit size=20, prob = 0.3
+yy <- rbinom(n = 1000, size =20, prob=0.3)
 
 # 3. Z: Simulieren Sie 100000 binomial verteilte Zufallsvariablen mit size=20, prob = 0.3
+zz <- rbinom(n = 10000000, size =20, prob=0.3)
+
+par(mfrow=c(1,3))
+hist(xx)
+hist(yy)
+hist(zz)
 
 
+# 1. X: Simulieren Sie 10 Poisson verteilte Zufallsvariablen mit lambda = 5
+set.seed(23487256)
+lam <- 5
+
+xx <- rpois(n = 10, lambda = lam)
+
+# 2. Y: Simulieren Sie 1000 Poisson verteilte Zufallsvariablen mit lambda = 5
+yy <- rpois(n = 1000, lambda = lam)
+
+# 3. Z: Simulieren Sie 100000 Poisson verteilte Zufallsvariablen mit lambda = 5
+zz <- rpois(n = 10000000, lambda = lam)
+
+par(mfrow=c(1,3))
+hist(xx)
+hist(yy)
+hist(zz)
 
--- scripts/exercise5_code.R
+++ scripts/exercise5_code.R
@@ -5,16 +5,30 @@
 # Aufgabe 1: X ist Poisson verteilt mit lambda = 10. 
 
 # Frage 1: Wie hoch ist die Wahrscheinlichkeit für X = 7?
-
+lam <- 10
 # 1) Berechnen Sie die Wahrscheinlichkeit mit Hilfe der Poisson Verteilung.
-# 2) Berechnen Sie die Wahrscheinlichkeit mit Hilfe der Normalverteilung. 
+dpois(x = 7, lambda = lam)
+
+# 2) Approximieren Sie die Wahrscheinlichkeit mit Hilfe der Normalverteilung. 
 # Nutzen Sie die Stetigkeitskorrektur
+
+# Folie 102, Approximationsvoraussetzungen sind erfüllt, lambda >= 10
+# diskret P(X = 7) --> Stetigkeitskorrektur P(6.5 <= X <= 7.5)  
+# P(6.5 <= X <= 7.5)  = P(X <= 7.5) - P(X <= 6.5)
+
+pnorm(7.5, mean = lam, sd = sqrt(lam)) - pnorm(6.5, mean = lam, sd = sqrt(lam))
 
 # Frage 2: Wie hoch ist die Wahrscheinlichkeit für X <= 7?
 
 # 1) Berechnen Sie die Wahrscheinlichkeit mit Hilfe der Poisson Verteilung.
+ppois(q = 7, lambda = lam)
+
 # 2) Berechnen Sie die Wahrscheinlichkeit mit Hilfe der Normalverteilung. 
 # Nutzen Sie die Stetigkeitskorrektur
+
+# Folie 102, Approximationsvoraussetzungen sind erfüllt, lambda >= 10
+# Stetigkeitskorrektur - P(X <= 7.5)
+pnorm(q = 7.5, mean = lam, sd = sqrt(lam))
 
 #-------------------------------------------------------------------------------
 
@@ -22,13 +36,44 @@
 
 # Frage 1: Wie hoch ist die Wahrscheinlichkeit für X = 13?
 
+n <- 20
+p <- 0.4
+
 # 1) Berechnen Sie die Wahrscheinlichkeit mit Hilfe der Binomialverteilung.
-# 2) Berechnen Sie die Wahrscheinlichkeit mit Hilfe der Normalverteilung. 
+dbinom(13, size=n, prob=p)
+
+# 2) Approximieren Sie die Wahrscheinlichkeit mit Hilfe der Normalverteilung. 
 # Nutzen Sie die Stetigkeitskorrektur
+# Stetigkeitskorrektung: P(12.5 <= X <= 13.5) 
+
+# Die Approximationsvoraussetzungen sind nicht erfüllt, darum darf man die 
+# Normalverteilung dafür nicht nutzen
+
+n*p*(1-p) # n*p*(1-p) muss >= 10 sein, Folie 102
+
+# Wenn man aber trotzdem die Normalverteilung nutzen würde, so sieht die Rechnung aus:
+pnorm(13.5, mean = n*p, sd = sqrt(n*p*(1-p))) - pnorm(12.5, mean = n*p, sd = sqrt(n*p*(1-p)))
+# In diesem Fall, wenn eine Punktwahrscheinlichkeit X = 13 approximiert wird, 
+# ist das Ergebnis durch die Normalverteilung nicht viel schlechter als
+# die echte Berechnung mit der Binomialverteilung
 
 # Frage 2: Wie hoch ist die Wahrscheinlichkeit für X zwischen 10 und 17?
 
 # 1) Berechnen Sie die Wahrscheinlichkeit mit Hilfe der Binomialverteilung.
-# 2) Berechnen Sie die Wahrscheinlichkeit mit Hilfe der Normalverteilung. 
+# P(10 < X < 17) = P(X <= 16) - P(X <= 10)
+pbinom(16, size=n, prob=p) - pbinom(10, size=n, prob=p)
+
+# 2) Approximieren Sie die Wahrscheinlichkeit mit Hilfe der Normalverteilung. 
 # Nutzen Sie die Stetigkeitskorrektur
 
+# Stetigkeitskorrektur: P(9.5 <= X <= 16.5), Folie 102
+
+# Die Approximationsvoraussetzungen sind nicht erfüllt, darum darf man die 
+# Normalverteilung dafür nicht nutzen
+n*p*(1-p) # n*p*(1-p) muss >= 10 sein.
+
+# Wenn man aber trotzdem die Normalverteilung nutzen würde, so sieht die Rechnung aus:
+pnorm(16.5, mean = n*p, sd = sqrt(n*p*(1-p))) - pnorm(9.5, mean = n*p, sd = sqrt(n*p*(1-p)))
+# -- > sehr großer Unterschied zu dem echten Berechnungswert mit der Binomialverteilung, 
+# weil die Approximationsvoraussetzunge nicht erfüllt sind, da ein großes Intervall
+# P(9.5 <= X <= 16.5) in die Berechnung eingeht.
